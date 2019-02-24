2beee626223eede1f76c6f786e00986b4c98b47f
Wee <fi3ework@gmail.com>
2019-02-25 02:24:47 +0800

    test: add basic test

--- __test__/array.test.ts
+++ __test__/array.test.ts
@@ -19,12 +19,7 @@ const obj = observable({
   skills: ['eat', 'sleep']
 })
 
-test('key/value access right', () => {
-  const o = observable(obj)
-  expect(o.name).toBe('Adam')
-  expect(o.family.father.name).toBe('daddy')
-  expect(o.pets[0].name).toBe('Cathy')
-  expect(o.pets.length).toBe(1)
-  expect(o.skills[0]).toBe('eat')
-  expect(o.skills.length).toBe(2)
+test('native method of array', () => {
+  obj.skills.push('code')
+  expect(obj.skills.slice()).toEqual(['eat', 'sleep'])
 })
