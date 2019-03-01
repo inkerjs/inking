d597bfc429ba626c18a6546ae7bca1a3a42bcd38
Wee <fi3ework@gmail.com>
2019-03-01 11:54:12 +0800

    test: add transaction test

--- __test__/action.test.ts
+++ __test__/action.test.ts
@@ -1,7 +1,7 @@
 import { action, autorun, observable } from '../src/index'
 import { buffer, getPlainObj } from './utils'
 
-test('transaction', () => {
+test('modify same property in transaction', () => {
   const obj = observable(getPlainObj())
   const b = buffer()
 
@@ -16,4 +16,35 @@ test('transaction', () => {
   })
 
   expect(b.toArray()).toEqual(['Adam', 't3'])
+})
+
+test('modify with native method', () => {
+  const obj = observable(getPlainObj())
+  const b1 = buffer()
+  const b2 = buffer()
+  const b3 = buffer()
+
+  autorun(() => {
+    b1(obj.skills.length)
+  })
+
+  autorun(() => {
+    b2(obj.skills[1])
+  })
+
+  autorun(() => {
+    b3(obj.skills.join('_'))
+  })
+
+  action(() => {
+    obj.skills.unshift('i1')
+    obj.skills.unshift('i2')
+    obj.skills.pop()
+    obj.skills.splice(0, 2, 'i3')
+    obj.skills.shift()
+  })
+
+  expect(b1.toArray()).toEqual([2, 1])
+  expect(b2.toArray()).toEqual(['sleep', undefined])
+  expect(b3.toArray()).toEqual(['eat_sleep', 'eat'])
 })
